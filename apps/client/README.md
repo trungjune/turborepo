69c0a70d049b75a4e3bc175095634f8dde1793fd
trungth <trungth@vnpt-technology.vn>
2024-10-15 17:57:15 +0700

    add source FE

--- apps/client/README.md
+++ apps/client/README.md
@@ -1,50 +1,17 @@
-# React + TypeScript + Vite
+### Source code frontend khóa học Nest.JS
+Tác giả: Hỏi Dân IT
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Các bước cài đặt: (chế độ development)
+1. clone code
+2. cài đặt thư viện: npm i
+3. Update file .env (nếu cần thiết)
+4. Chạy dự án: npm run dev
 
-Currently, two official plugins are available:
+===
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
-
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default tseslint.config({
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
-})
-```
-
-- Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked` or `tseslint.configs.strictTypeChecked`
-- Optionally add `...tseslint.configs.stylisticTypeChecked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and update the config:
-
-```js
-// eslint.config.js
-import react from 'eslint-plugin-react'
-
-export default tseslint.config({
-  // Set the react version
-  settings: { react: { version: '18.3' } },
-  plugins: {
-    // Add the react plugin
-    react,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended rules
-    ...react.configs.recommended.rules,
-    ...react.configs['jsx-runtime'].rules,
-  },
-})
-```
+Cách chạy tại chế độ production:
+1. clone code
+2. cài đặt thư viện: npm i
+3. Update file .env (nếu cần thiết)
+4. Build dự án: npm run build
+5. Chạy dự án: npm run preview
--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,52 +1,166 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  useLocation,
+} from "react-router-dom";
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import NotFound from 'components/share/not.found';
+import Loading from 'components/share/loading';
+import LoginPage from 'pages/auth/login';
+import RegisterPage from 'pages/auth/register';
+import LayoutAdmin from 'components/admin/layout.admin';
+import ProtectedRoute from 'components/share/protected-route.ts';
+import Header from 'components/client/header.client';
+import Footer from 'components/client/footer.client';
+import HomePage from 'pages/home';
+import styles from 'styles/app.module.scss';
+import DashboardPage from './pages/admin/dashboard';
+import CompanyPage from './pages/admin/company';
+import PermissionPage from './pages/admin/permission';
+import ResumePage from './pages/admin/resume';
+import RolePage from './pages/admin/role';
+import UserPage from './pages/admin/user';
+import { fetchAccount } from './redux/slice/accountSlide';
+import LayoutApp from './components/share/layout.app';
+import JobPage from './pages/admin/job';
+import ViewUpsertJob from './components/admin/job/upsert.job';
+import ClientJobPage from './pages/job';
+import ClientJobDetailPage from './pages/job/detail';
+import ClientCompanyPage from './pages/company';
+import ClientCompanyDetailPage from './pages/company/detail';
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [serverResponse, setServerResponse] = useState('');
+const LayoutClient = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const location = useLocation();
+  const rootRef = useRef<HTMLDivElement>(null);
 
-  const checkServerConnection = async () => {
-    try {
-      const response = await fetch('/api/v1'); 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.text();
-      setServerResponse(data); // Lưu kết quả trả về từ server
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
-      setServerResponse('Error: Could not connect to server');
+  useEffect(() => {
+    if (rootRef && rootRef.current) {
+      rootRef.current.scrollIntoView({ behavior: 'smooth' });
     }
-  };
+
+  }, [location]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='layout-app' ref={rootRef}>
+      <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <div className={styles['content-app']}>
+        <Outlet context={[searchTerm, setSearchTerm]} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <button onClick={checkServerConnection}>Ping Server</button>
-      <p>Server response: {serverResponse}</p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Footer />
+    </div>
   )
 }
 
-export default App
+export default function App() {
+  const dispatch = useAppDispatch();
+  const isLoading = useAppSelector(state => state.account.isLoading);
+
+
+  useEffect(() => {
+    if (
+      window.location.pathname === '/login'
+      || window.location.pathname === '/register'
+    )
+      return;
+    dispatch(fetchAccount())
+  }, [])
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (<LayoutApp><LayoutClient /></LayoutApp>),
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "job", element: <ClientJobPage /> },
+        { path: "job/:id", element: <ClientJobDetailPage /> },
+        { path: "company", element: <ClientCompanyPage /> },
+        { path: "company/:id", element: <ClientCompanyDetailPage /> }
+      ],
+    },
+
+    {
+      path: "/admin",
+      element: (<LayoutApp><LayoutAdmin /> </LayoutApp>),
+      errorElement: <NotFound />,
+      children: [
+        {
+          index: true, element:
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+        },
+        {
+          path: "company",
+          element:
+            <ProtectedRoute>
+              <CompanyPage />
+            </ProtectedRoute>
+        },
+        {
+          path: "user",
+          element:
+            <ProtectedRoute>
+              <UserPage />
+            </ProtectedRoute>
+        },
+
+        {
+          path: "job",
+          children: [
+            {
+              index: true,
+              element: <ProtectedRoute> <JobPage /></ProtectedRoute>
+            },
+            {
+              path: "upsert", element:
+                <ProtectedRoute><ViewUpsertJob /></ProtectedRoute>
+            }
+          ]
+        },
+
+        {
+          path: "resume",
+          element:
+            <ProtectedRoute>
+              <ResumePage />
+            </ProtectedRoute>
+        },
+        {
+          path: "permission",
+          element:
+            <ProtectedRoute>
+              <PermissionPage />
+            </ProtectedRoute>
+        },
+        {
+          path: "role",
+          element:
+            <ProtectedRoute>
+              <RolePage />
+            </ProtectedRoute>
+        }
+      ],
+    },
+
+
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
